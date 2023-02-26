f0e5920a2bd2756d8a399af51ca03ddff7e732f4
esby-space <seabass.branch@gmail.com>
2023-02-26 18:55:59 -0500

    chapter 1 half finished

---
+++ astro.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config"
+import remarkMath from "remark-math"
+import rehypeKatex from "rehype-katex";
+import rehypeMathJax from "rehype-mathjax"
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://esby-space.github.io",
+    base: "/spirographs",
+    integrations: [mdx()],
+    markdown: {
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeMathJax]
+    }
+});
